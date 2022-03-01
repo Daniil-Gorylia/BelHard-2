382fb2f51293c7452f8efad62c7b62f899a2bae0
Daniil <zzzgahihzzz@gmail.com>
2022-03-01 17:08:50 +0300

    a

---
+++ main.py
@@ -0,0 +1,59 @@
+name = 'Daniil'
+age = 21
+lastname = 'Gorylia'
+print(f'Hello, my name is {name} {lastname}, my age is {age}')
+
+my_object = None
+
+print(my_object)
+print(type(my_object))
+
+
+my_object = 1
+
+print(my_object)
+print(type(my_object))
+
+my_object = 'name'
+
+print(my_object)
+print(type(my_object))
+my_object = 1.5
+
+print(my_object)
+print(type(my_object))
+
+my_object = (1+2j)
+
+print(my_object)
+print(type(my_object))
+
+my_object = (1, 2, 3)
+
+print(my_object)
+print(type(my_object))
+
+my_object = {1, 2, 3}
+
+print(my_object)
+print(type(my_object))
+
+my_object = {}
+
+print(my_object)
+print(type(my_object))
+
+my_object = [1, 2, 3]
+
+print(my_object)
+print(type(my_object))
+
+my_object = b'b123'
+
+print(my_object)
+print(type(my_object))
+
+my_object = bytearray(b'Hello world!')
+
+print(my_object)
+print(type(my_object))
